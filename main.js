7ed4a70a8745a74bcf17c00da7d51fd7eb43965f
Kalle Tiihonen <kalletiihonen@hotmail.com>
2022-05-02 22:29:46 +0200

    burger menu working, not 100% styled

--- main.js
+++ main.js
@@ -3,7 +3,12 @@ import './sass/all_styles.scss';
 // import './sass/style.scss';
 import { startAnimations } from './modules/animations.js'
 new ClipboardJS('.copy_email');
+const menuToggle = document.querySelector(".menuToggle");
+const navigation = document.querySelector(".burger");
 
+menuToggle.onclick = () => {
+    navigation.classList.toggle("active");
+}
 
 window.addEventListener("DOMContentLoaded", start);
 
@@ -15,6 +20,7 @@ function start() {
     // }
     startAnimations();
     topFunction();
+    burgerMenu();
 }
 
 function topFunction() {
